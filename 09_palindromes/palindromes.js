3cfcb260ba2162052224dd694598f705fbde81d8
Evan Mastro <mastroevan@gmail.com>
2024-08-06 21:13:27 -0700

    excercise 9 complete, palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanedStr = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    return cleanedStr === cleanedStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
