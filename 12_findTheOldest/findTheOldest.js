51d504915db88bb376e39b8a6dd9e2343d3d485b
Evan Mastro <mastroevan@gmail.com>
2024-08-06 21:19:44 -0700

    finished through excercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
-};
-
+// findTheOldest.js
+const findTheOldest = (people) => {
+    const currentYear = new Date().getFullYear();
+  
+    return people.reduce((oldest, person) => {
+      const ageOfCurrentPerson = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+      const ageOfOldestPerson = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+  
+      return ageOfCurrentPerson > ageOfOldestPerson ? person : oldest;
+    });
+  };
+  
 // Do not edit below this line
 module.exports = findTheOldest;
